a3776ef30205652e9ae2b2a73762764c77d06c6e
transferXiang <448217518@qq.com>
2015-08-06 00:24:02 +0800

    添加百度地图的SDK
    1、添加SDK
    2、创建一个初始的地图界面

--- src/com/baidumap/tdesign/MainActivity.java
+++ src/com/baidumap/tdesign/MainActivity.java
@@ -1,17 +1,46 @@
 package com.baidumap.tdesign;
 
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	MapView mMapView = null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext  
+        //注意该方法要再setContentView方法之前实现  
+        SDKInitializer.initialize(getApplicationContext()); 
+        
         setContentView(R.layout.activity_main);
+        
+        mMapView = (MapView) findViewById(R.id.bmapView);
     }
 
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+    	mMapView.onDestroy();
+    }
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	mMapView.onPause();
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	mMapView.onResume();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
