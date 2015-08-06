14cf2bf47dfc86b1bfdbe87fcfd87df6631a7440
transferXiang <448217518@qq.com>
2015-08-06 23:57:20 +0800

    在菜单中添加选项，用于选择不同的地图类型

--- src/com/baidumap/tdesign/MainActivity.java
+++ src/com/baidumap/tdesign/MainActivity.java
@@ -2,14 +2,18 @@ package com.baidumap.tdesign;
 
 
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 	MapView mMapView = null;
+	BaiduMap mBaiduMap = null;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
     }
 
     @Override
@@ -49,4 +54,27 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+		case R.id.id_map_common:
+			mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+			break;
+
+		case R.id.id_map_satellite:
+			mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+			break;
+			
+		case R.id.id_map_traffic:
+			boolean enable = mBaiduMap.isTrafficEnabled();
+			mBaiduMap.setTrafficEnabled(!enable);
+			item.setTitle(enable ? R.string.map_traffic_off : R.string.map_traffic_on);
+			break;
+			
+		default:
+			Log.d("MainActivity.onOptionsItemSelected", "unknown item type");
+			break;
+		}
+    	return super.onOptionsItemSelected(item);
+    }
 }
