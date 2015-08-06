d122b2b821ba17d6ce92373902b6977464f9611f
transferXiang <448217518@qq.com>
2015-08-07 00:54:34 +0800

    添加基础的定位功能

--- src/com/baidumap/tdesign/MainActivity.java
+++ src/com/baidumap/tdesign/MainActivity.java
@@ -1,19 +1,36 @@
 package com.baidumap.tdesign;
 
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	MapView mMapView = null;
-	BaiduMap mBaiduMap = null;
+	private MapView mMapView = null;
+	private BaiduMap mBaiduMap = null;
+	
+	private LocationClient mLocaltionClient;
+	private MyLocationListenr mLocationListener;
+	private boolean isFirstIn = true;
+	
+	Context context;
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +40,23 @@ public class MainActivity extends Activity {
         //注意该方法要再setContentView方法之前实现  
         SDKInitializer.initialize(getApplicationContext()); 
         
+        context = this;
+        
         setContentView(R.layout.activity_main);
         
         mMapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
+        
+        mLocaltionClient = new LocationClient(this);
+        mLocationListener = new MyLocationListenr();
+        mLocaltionClient.registerLocationListener(mLocationListener);
+        
+        LocationClientOption option = new LocationClientOption();
+        option.setCoorType("bd09ll");
+        option.setIsNeedAddress(true);
+        option.setOpenGps(true);
+        option.setScanSpan(1000); //1s中扫描一次
+        mLocaltionClient.setLocOption(option);
     }
 
     @Override
@@ -45,6 +75,27 @@ public class MainActivity extends Activity {
     protected void onResume() {
     	super.onResume();
     	mMapView.onResume();
+    }
+    
+    @Override
+    protected void onStart() {
+    	// 开启定位
+    	mBaiduMap.setMyLocationEnabled(true);
+    	if(!mLocaltionClient.isStarted()){
+    		mLocaltionClient.start();
+    	}
+    	
+    	super.onStart();
+    }
+    
+    @Override
+    protected void onStop() {
+    	// 停止定位
+    	if(mLocaltionClient.isStarted()){
+    		mLocaltionClient.stop();
+    	}
+    	mBaiduMap.setMyLocationEnabled(false);
+    	super.onStop();
     }
 
     @Override
@@ -76,5 +127,31 @@ public class MainActivity extends Activity {
 			break;
 		}
     	return super.onOptionsItemSelected(item);
+    }
+    
+    
+    private class MyLocationListenr implements BDLocationListener
+    {
+
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			MyLocationData data = new MyLocationData.Builder()//
+			.accuracy(location.getRadius())//
+			.latitude(location.getLatitude())//
+			.longitude(location.getLongitude())//
+			.build();
+			mBaiduMap.setMyLocationData(data);
+			
+			// 第一次进入应用时才进行定位
+			if(isFirstIn){
+				LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+				MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+				mBaiduMap.animateMapStatus(msu);
+				
+				isFirstIn = false;
+				Toast.makeText(context, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+			}
+		}
+    	
     }
 }
